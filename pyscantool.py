935316788b1e8be2ad46db3af6ba560de71ae19a
IchiBan360 <karoliszilionis@gmail.com>
2023-02-20 13:30:41 +0200

    sutvarkyta email sistema

--- pyscantool.py
+++ pyscantool.py
@@ -2,8 +2,9 @@ import subprocess
 import configparser
 import os.path
 import shutil
-import difflib
+import filecmp
 import sys
+import yagmail
 
 print('dns scenavimo irankis')
 
@@ -22,8 +23,8 @@ if os.path.exists(testOut):
 
 #Testavimo budo nuskaitymas is config.ini failo
 config.read(configFilePath)
-test = config['test-case']['testcase']
-print('Pasirinktas skenavimo budas:',test)
+tests = config['test-cases']['tests'].split(',')
+print('Pasirinktas skenavimo budas:',tests)
 
 domains = config['domain-names']['domains'].split(',')
 for domain in domains:
@@ -31,23 +32,26 @@ for domain in domains:
     fd = open(testOut, 'a')
     fd.writelines(domain + '\n')
     fd.close()
-    fd = open(testOut, 'a')
-    subprocess.run([cmd, domain, '--test', test], stdout=fd)
-    fd.writelines('\n')
-    fd.close()
+    for test in tests:
+        fd = open(testOut, 'a')
+        subprocess.run([cmd, domain, '--test', test], stdout=fd)
+        fd.writelines('\n')
+        fd.close()
 
-with open(testOut, 'r') as f1:
-    with open(testOld, 'r') as f2:
-        diff = difflib.unified_diff(
-                f1.readlines(),
-                f2.readlines(),
-                fromfile='f1',
-                tofile='f2',
-        )
-        for line in diff:
-            print(line)
+if filecmp.cmp(testOut, testOld):
+    body = 'Nauju klaidu nerasta'
+    print('nauju klaidu nerasta')
+else:
+    body = 'Rasta nauju klaidu!'
 
-
-# closing files
-f1.close()                                      
-f2.close()  
+#kolkas uzkomentuoju kad kaskart email nesiustu
+print('siunciaum email')
+receivers = config['email-receivers']['receivers'].split(',')
+for receiver in receivers:
+    yag = yagmail.SMTP('ichiban360@gmail.com', 'mhiwcixrszpuhjdz')
+    yag.send(
+            to=receiver,
+            subject='Domenu skenavimo rezultatai',
+            contents=body,
+            attachments=testOut,
+    )
